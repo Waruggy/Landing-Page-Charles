7e109d84741881b64f6d6bc35fa9af36da1d9301
Charles <cwang8413@yahoo.com>
2019-06-06 20:32:30 -0700

    More parts

---
+++ components/Carousel/Carousel.js
@@ -0,0 +1,67 @@
+class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.lbtn = this.carousel.querySelector('.left-button');
+        this.rbtn = this.carousel.querySelector('.right-button');
+        this.img = this.carousel.querySelectorAll('img');
+        this.prevImg = this.currentIndex;
+        this.zero = zero;
+        this.one = one;
+        this.two = two;
+        this.three = three;
+        this.currentIndex = 0;
+        this.zero.addEventListener('click', () => {
+            this.currentIndex = 0, this.changeImg()
+        })
+        this.one.addEventListener('click', () => {
+            this.currentIndex = 1, this.changeImg()
+        })
+        this.two.addEventListener('click', () => {
+            this.currentIndex = 2, this.changeImg()
+        })
+        this.three.addEventListener('click', () => {
+            this.currentIndex = 3, this.changeImg()
+        })
+        this.lbtn.addEventListener('click', () => {
+            this.leftButton()
+        })
+        this.rbtn.addEventListener('click', () => {
+            this.rightButton()
+        })
+        this.img.forEach(x => (x.style.display = "none"))
+        this.img[this.currentIndex].style.display = "block";
+    }
+
+    leftButton() {
+        this.img.forEach(x => (x.style.display = "none"));
+        if (this.currentIndex == this.img.length - 1) {
+            this.currentIndex = 0;
+            this.img[this.currentIndex].style.display = "block";
+        } else {
+            this.currentIndex++;
+            this.img[this.currentIndex].style.display = "block";
+        }
+    }
+    rightButton() {
+        this.img.forEach(x => (x.style.display = "none"));
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.img.length - 1;
+            this.img[this.currentIndex].style.display = "block";
+        } else {
+            this.currentIndex--;
+            this.img[this.currentIndex + 1].style.display = "block";
+        }
+    }
+    changeImg() {
+        this.img.forEach(x => (x.style.display = "none"));
+        this.img[this.currentIndex].style.display = "block"
+    }
+}
+
+
+let carousel = document.querySelector('.carousel');
+let zero = document.querySelector('.index0')
+let one = document.querySelector('.index1')
+let two = document.querySelector('.index2')
+let three = document.querySelector('.index3')
+new Carousel(carousel);
